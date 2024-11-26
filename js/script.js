b2588ed7d4d78ce4989ae40c71f236edd9f8a9ba
Alexpra2 <alexpra2@gmail.com>
2024-11-26 23:44:53 +0100

    addinnerHTML

--- js/script.js
+++ js/script.js
@@ -1,34 +1,52 @@
 
 
 fetch(`https://jsonplaceholder.typicode.com/users/`)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('No Funciona la pagina')
-    }
-    return response.json();
-})
-.then((data) => {                         //vambiar data por users
-    console.log(data);
-    obtenerusuario(data);
-    
-});
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('No Funciona la pagina')
+        }
+        return response.json();
+    })
+    .then((data) => {                         //vambiar data por users
+        console.log(data);
+        obtenerusuario(data);
+    });
 
 
 function obtenerusuario(users) {
     const lista = document.getElementById('listaUsuarios');
     lista.innerHTML = '';
-       
+
     users.forEach((user, i) => {
         const li = document.createElement('li');
-        
-        // Contenedor Datos/Imagen 
+        li.innerHTML = `
+        <div class="contenedorPerfil">
+            <div class="divDatosPersonales">
+                <h3>Name: ${user.name}</h3>
+                <h3>Edad: ${Math.floor(Math.random() * (60 - 18 + 1)) + 18}</h3>
+                <h3>Username: ${user.username}</h3>
+                <h3>Phone: ${user.phone}</h3>
+                <h3>Email: ${user.email}</h3>
+            </div>
+            <div class="divImagen">
+                <img src="/assets/img/${i + 1}.jpeg" alt="Foto de Usuario" />
+            </div>
+        </div>
+        <div class="contenedorPerfil">
+            <div class="divDireccion">
+                <h3>Compañia: ${user.company.name}</h3>
+                <h3>Dirección: ${user.address.street} ${user.address.suite} ${user.address.city}</h3>
+            </div>
+        </div>`;
+
+        /* // Contenedor Datos/Imagen 
         const perfil = document.createElement('div');
         perfil.classList.add('contenedorPerfil');
 
         /// Contenedor datos personales
         const divDatos = document.createElement('div');                    //hacelo con .innerHTML challenge U1S5 1:30
         divDatos.classList.add('divDatosPersonales');
-        
+
         const name = document.createElement('h3');
         name.textContent = "Name: " + user.name;
         divDatos.appendChild(name);
@@ -36,7 +54,7 @@ function obtenerusuario(users) {
         const edadAleatoria = document.createElement('h3');
         edadAleatoria.textContent = `Edad: ${Math.floor(Math.random() * (60 - 18 + 1)) + 18}`;
         divDatos.appendChild(edadAleatoria);
-        
+
         const username = document.createElement('h3');
         username.textContent = "Username: " + user.username;
         divDatos.appendChild(username);
@@ -47,24 +65,24 @@ function obtenerusuario(users) {
 
         const email = document.createElement('h3');
         email.textContent = "Email: " + user.email;
-        divDatos .appendChild(email);
-        
+        divDatos.appendChild(email);
+
         //Contenedor imagen
 
         const divImagen = document.createElement('div');
         divImagen.classList.add('divImagen');
         const img = document.createElement('img');
-        img.src = `users/assets/img/${i+1}.jpeg`; 
-        img.alt = 'Foto de Usuario' ;                     
+        img.src = `/assets/img/${i + 1}.jpeg`;
+        img.alt = 'Foto de Usuario';
         divImagen.appendChild(img);
-        
+
         //Incluyo contenedor datos personales e imagen en Contenedor perfil
-        
+
         perfil.appendChild(divDatos);
         perfil.appendChild(divImagen);
 
         //contenedor Compañia/direccion
-        
+
         const direccion = document.createElement('div');
         direccion.classList.add('divDireccion');
 
@@ -73,18 +91,18 @@ function obtenerusuario(users) {
         direccion.appendChild(company);
 
         const address = document.createElement('h3');
-        address.textContent = `Dirección: ${user.address.street} ${user.address.suite} ${user.address.city}`; 
+        address.textContent = `Dirección: ${user.address.street} ${user.address.suite} ${user.address.city}`;
         direccion.appendChild(address);
-        
+
         //incluyo todo en el li
-        li.appendChild(perfil);
-        li.appendChild(direccion);
-        
+        //li.appendChild(perfil);
+        li.appendChild(direccion); */
+
         //incluyo el li en el ul
         lista.appendChild(li);
 
     })
-    
-} 
+
+}
 
 
